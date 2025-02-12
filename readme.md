23feadd08ed543198e30bd03122bcb8c4f9c3a84
kouyama <mkoyama@iim.co.jp>
2025-02-12 09:48:48 +0900

    とりあえず整理

--- readme.md
+++ readme.md
@@ -2,6 +2,37 @@
 
 PowerShellからSeleniumを起動する方法についてまとめる。
 
+## ディレクトリ構造
+c:\selenuim
+
+C:\selenium\chrome.exe
+ テスト用chrome本体
+ https://googlechromelabs.github.io/chrome-for-testing/
+ 　よりダウンロード
+ 
+ C:\selenium\script
+　スクリプト
+C:\selenium\Selenium.WebDriver.4.28.0
+https://www.selenium.dev/downloads/
+の .net/c# より取得
+nuget.exe を取得して
+>nuget install Selenium.WebDriver
+>nuget install Selenium.Support
+>nuget install Selenium.WebDriver.
+ChromeDriver
+
+としたほうがいいかも。
+C:\selenium\Selenium.WebDriver.4.28.0\lib
+から、「net8.0」を削除
+
+C:\selenium\Selenium.Support.4.28.0
+必要ないかも,,
+
+
+
+
+  
+
 * Windows10/Windows2019サーバの場合、標準のPowerShellが、v5.0となっているため、v7を参照する必要あり *
   
 これではまった。Visual Studio Codeだとうまく行くがコマンドラインから実行するとエラーになっていた。
--- run.ps1
+++ run.ps1
@@ -3,45 +3,38 @@
 # 以下のサイトを参照
 # https://seikogroup.sharepoint.com/:v:/s/IIMSS/EbOWS6xFU4pPsBhUsTX7FhoBG9Aw4mFUbZzL3yr6vRhkNg?e=5sm7Yg
 # https://adamtheautomator.com/selenium-and-powershell/
-#SParam($Username,$passoword,$url,$Username_xpath,$passoword_xpath,$click_name)
-$Username="mkoyama@iim.co.jp"
-$password="mkoyama@iim.co.jp"
-$url="https://github.com/login"
-$Username_xpath="//*[@id='login_field']"
-$passoword_xpath="//*[@id='''password']"
-$click_name="commit"
-# if ($Username -eq "") {$Username="mkoyama@iim.co.jp"}
-# if ($password -eq "") {$password="mkoyama@iim.co.jp"}
-# if ($url -eq "") {$url="https://github.com/login"}
-# if ($Username_xpath -eq "") {$Username_xpath="//*[@id='login_field']"}
-# if ($passoword_xpath -eq "") {$passoword_xpath="//*[@id='password']"}
-# if ($click_name -eq "") {$click_name="commit"}
-Write-Host $Username
-Write-Host $url
-Write-Host $Username_xpath
-Write-Host $passoword_xpath
+Param(
+    [string]$Username= "mkoyama@iim.co.jp",
+    [string]$Pass="mkoyama@iim.co.jp",
+    [string]$url="https://github.com/login"
+    # [string]$Username_xpath="//*[@id='login_field']",
+    # [string]$passoword_xpath="//*[@id='login_field']",
+    # [string]$click_name="commit"
+    )
 # see https://www.kadinche.com/tech-blog/powershell-e3-81-a7selenium-webdriver-e3-82-92-e4-bd-bf-e3-81-86-e3-81-9f-e3-82-81-e3-81-ae-e5-8b-98-e6-89-80
-$workingPath = 'C:\selenium'
+# $workingPath = 'C:\selenium'
 $seleniumHome = 'C:\selenium'
-$webDriverDllPath
-$webDriverSupportDllPath
-$webDriverDllPath = Convert-Path (Join-Path $seleniumHome "\Selenium.WebDriver.4.28.0\lib\netstandard2.0\WebDriver.dll")
-$webDriverSupportDllPath = Convert-Path (Join-Path $seleniumHome "\Selenium.Support.4.28.0\lib\netstandard2.0\WebDriver.Support.dll")
+# DLLを読み込む
+$webDriverDllPath=(Get-ChildItem -Path $seleniumHome -Filter WebDriver.dll -Recurse).FullName
+$webDriverSupportDllPath=(Get-ChildItem -Path $seleniumHome -Filter WebDriver.Support.dll -Recurse).FullName
 Add-Type -Path $webDriverDllPath
 Add-Type -Path $webDriverSupportDllPath
 
-# Add the working directory to the environment path.
-# This is required for the ChromeDriver to work.
-if (($env:Path -split ';') -notcontains $workingPath) {
-    $env:Path += ";$workingPath"
+# Chomeに対してバスをとおす
+$chome_dir = (Get-ChildItem -Path $seleniumHome  -Filter chrome.exe -Recurse).DirectoryName  
+if (($env:Path -split ';') -notcontains $chome_dir){
+    $env:Path += ";$chome_dir"
 }
+# if (($env:Path -split ';') -notcontains $workingPath) {
+#     $env:Path += ";$workingPath"
+# }
 $ChromeDriver = New-Object OpenQA.Selenium.Chrome.ChromeDriver
 # GoogleだとログインアイコンをクリックできないためGitに変更
 $ChromeDriver.Navigate().GoToURL($url)
 # ログイン
 # $ChromeDriver.FindElement([OpenQa.Selenium.By]::xPath("$Username_xpath").SendKeys("$Username"))
-# $ChromeDriver.FindElement([OpenQa.Selenium.By]::xpath("$passoword_xpath").SendKeys("$password"))
+# $ChromeDriver.FindElement([OpenQa.Selenium.By]::xpath("$passoword_xSSpath").SendKeys("$password"))
 # $ChromeDriver.FindElement([OpenQa.Selenium.By]::name("$click_name").click())
 $ChromeDriver.FindElement([OpenQa.Selenium.By]::xPath("//*[@id='login_field']")).SendKeys($Username)
-$ChromeDriver.FindElement([OpenQa.Selenium.By]::xpath('//*[@id="password"]')).SendKeys($password)
+$ChromeDriver.FindElement([OpenQa.Selenium.By]::xpath('//*[@id="password"]')).SendKeys($pass)
 $ChromeDriver.FindElement([OpenQa.Selenium.By]::name('commit')).click()
